9831d6963fddb5018efcd58dd2089baf2f3501d3
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-15 08:44:19 +0900

    remove: lesson35 starting file.
    https://gitmoji.dev/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import styled from 'styled-components/macro';
-import { DefaultButton } from './components/Buttons';
-
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <DefaultButton>click me</DefaultButton>
-      <DefaultButton large>click me</DefaultButton>
+      <h2>attrs method</h2>
     </div>
   );
 }

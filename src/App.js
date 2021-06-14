2973fc1fc5088814fa376aa1129ee3de7534f1c8
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-14 10:34:43 +0900

    update: lesson10
    https://gitmoji.dev/

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ function App() {
     <div style={{ padding: '2rem' }}>
       <h1 className="title">styled components</h1>
       <button className="btn">click me</button>
+      <div style={{ padding: '2rem' }}>
+        <h2 className="section-title">hello world</h2>
+      </div>
     </div>
   );
 }

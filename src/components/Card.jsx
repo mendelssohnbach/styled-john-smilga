52fa35543ca2e204025bb829c29f4ebd582420d1
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-14 18:52:24 +0900

    :lipstick: lesson26 | Card UI
    https://gitmoji.dev/

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Card = () => {
   return (
-    <div>
+    <StyledCard>
       <img
         src="https://dl.airtable.com/.attachmentThumbnails/0be1af59cf889899b5c9abb1e4db38a4/d631ac52"
         alt="albany sectional"
@@ -11,10 +11,20 @@ const Card = () => {
         <h4>product name</h4>
         <p>$109999</p>
       </footer>
-    </div>
+    </StyledCard>
   );
 };
 
-const StyledCard = styled.article``;
+const StyledCard = styled.article`
+  width: 90vw;
+  max-width: 300px;
+  background: var(--white);
+  border-radius: 0.25rem;
+  img {
+    width: 100%;
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
+  }
+`;
 
 export default Card;

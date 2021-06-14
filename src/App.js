be7a1631ab3a1fd64b63582493aee784edab52f6
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-14 18:46:47 +0900

    update: lesson25
    https://gitmoji.dev/

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Card from './components/Card';
+
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h2>card will sit here</h2>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Card = () => {
+  return (
+    <div>
+      <img
+        src="https://dl.airtable.com/.attachmentThumbnails/0be1af59cf889899b5c9abb1e4db38a4/d631ac52"
+        alt="albany sectional"
+      />
+      <footer>
+        <h4>product name</h4>
+        <p>$109999</p>
+      </footer>
+    </div>
+  );
+};
+
+const StyledCard = styled.article``;
+
+export default Card;

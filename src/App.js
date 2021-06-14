7c19db82ba868fcfec6468a141d180fe87901d17
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-15 05:49:38 +0900

    update: lesson29
    https://gitmoji.dev/

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Card from './components/Card';
+import GlobalStyles from './global-styles';
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
+      <GlobalStyles />
       <Card />
     </div>
   );

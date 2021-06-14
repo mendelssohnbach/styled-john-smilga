ade4aad4b714a6546024bfd98a2c486b4366c124
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-14 12:16:11 +0900

    update: lesson13
    https://gitmoji.dev/

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import styled from 'styled-components';
 
 const BasicTitle = styled.h1`
-  text-align: right;
+  text-align: center;
   text-transform: capitalize;
+`;
+
+const DefaultButton = styled.button`
+  background: #645cff;
+  color: #fff;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  text-transform: capitalize;
+  padding: 0.25rem;
+  display: block;
+  width: 200px;
+  margin: 1rem auto;
 `;
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
       <BasicTitle>styled components</BasicTitle>
-      <BasicTitle>styled components</BasicTitle>
-      <BasicTitle>styled components</BasicTitle>
-      <BasicTitle>styled components</BasicTitle>
-      <button className="btn">click me</button>
+      <DefaultButton className="btn">click me</DefaultButton>
     </div>
   );
 }

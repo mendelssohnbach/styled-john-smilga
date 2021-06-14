287fc21d80d612cd13c6031b80b9019ab0baac42
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-15 08:30:15 +0900

    update: lesson33
    https://gitmoji.dev/

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-// import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import { HipsterButton } from './components/Buttons';
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
       <HipsterButton>click me</HipsterButton>
-      <HipsterButton as="a" href="https://www.johnsmilga.com">
+      <HipsterButton
+        css={`
+          color: green;
+        `}
+        as="a"
+        href="https://www.johnsmilga.com"
+      >
         click me
       </HipsterButton>
       <HipsterButton>click me</HipsterButton>
+      <div
+        css={`
+          color: green;
+        `}
+      >
+        <h2>hello world</h2>
+      </div>
     </div>
   );
 }
